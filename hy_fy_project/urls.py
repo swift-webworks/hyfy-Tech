1cbab045058b73b9c5762c0e0c7af8a473026d30
swift-webworks <swiftwebworks05@gmail.com>
2026-09-05 19:45:31 +0530

    Serve media files in production

--- hy_fy_project/urls.py
+++ hy_fy_project/urls.py
@@ -1,8 +1,8 @@
 from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path, include, re_path
+from django.views.static import serve
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
-from django.urls import include, path
 from django.views.generic import TemplateView
 
 from .sitemaps import ServiceSitemap, StaticViewSitemap
@@ -37,5 +37,10 @@ urlpatterns = [
 handler404 = "core.views.error_404"
 handler500 = "core.views.error_500"
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(
+        r"^media/(?P<path>.*)$",
+        serve,
+        {"document_root": settings.MEDIA_ROOT},
+    ),
+]
